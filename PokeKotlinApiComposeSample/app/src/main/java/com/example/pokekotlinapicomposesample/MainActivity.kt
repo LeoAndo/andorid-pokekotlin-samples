a82c920b84e37f0d51c2df3d6d64f0e7f489238c
LeoAndo <leo.777.info@gmail.com>
2022-01-24 03:53:18 +0900

    add Compose project.

---
+++ PokeKotlinApiComposeSample/app/src/main/java/com/example/pokekotlinapicomposesample/MainActivity.kt
@@ -0,0 +1,185 @@
+package com.example.pokekotlinapicomposesample
+
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
+import com.example.pokekotlinapicomposesample.ui.theme.PokeKotlinApiComposeSampleTheme
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    private val viewModel by viewModels<PokemonViewModel>()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PokeKotlinApiComposeSampleTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    PokemonScreen(viewModel)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun PokemonScreen(viewModel: PokemonViewModel) {
+    var pokemonIdText by remember { mutableStateOf("") }
+    var isEnableButton by remember { mutableStateOf(false) }
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+    Scaffold(
+        snackbarHost = { SnackbarHost(snackbarHostState) },
+        content = { innerPadding ->
+            PokemonContent(
+                uiState = viewModel.uiState,
+                pokemonIdText = pokemonIdText,
+                isEnableButton = isEnableButton,
+                onValueChange = {
+                    pokemonIdText = it
+                    isEnableButton = it.isNotEmpty()
+                },
+                onClickSearchButton = {
+                    viewModel.searchPokemon(pokemonIdText)
+                },
+                showSnackBarMessage = {
+                    // show snackbar as a suspend function
+                    scope.launch { snackbarHostState.showSnackbar(it) }
+                }
+            )
+        }
+    )
+}
+
+@OptIn(ExperimentalCoilApi::class)
+@Composable
+fun PokemonContent(
+    uiState: UiState,
+    pokemonIdText: String,
+    isEnableButton: Boolean,
+    onValueChange: (String) -> Unit,
+    onClickSearchButton: () -> Unit,
+    showSnackBarMessage: (String) -> Unit
+) {
+    Column(
+        modifier = Modifier.padding(12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        TextField(modifier = Modifier.fillMaxWidth(),
+            value = pokemonIdText,
+            onValueChange = onValueChange,
+            label = {
+                Text(text = "input here Pokemon ID...")
+            })
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = onClickSearchButton,
+            enabled = isEnableButton
+        ) {
+            Text(text = "Search!!!")
+        }
+
+        when (uiState) {
+            UiState.Initial -> {}
+            is UiState.Error -> {
+                showSnackBarMessage(uiState.errorMessage)
+            }
+            is UiState.PokemonData -> {
+                Image(
+                    painter = rememberImagePainter(
+                        data = uiState.url,
+                        builder = {
+                            crossfade(true)
+                            placeholder(drawableResId = android.R.drawable.btn_star)
+                        }
+                    ),
+                    contentDescription = "Pokemon Image",
+                    modifier = Modifier.size(300.dp),
+                    contentScale = ContentScale.Crop,
+                )
+                Text(text = "${uiState.id} : ${uiState.name}")
+            }
+        }
+    }
+}
+
+@Preview(
+    "default: Error",
+    showBackground = true,
+    uiMode = UI_MODE_NIGHT_NO,
+    device = Devices.PIXEL_4
+)
+@Preview(
+    "dark theme: Error",
+    showBackground = true,
+    uiMode = UI_MODE_NIGHT_YES,
+    device = Devices.PIXEL_4
+)
+@Composable
+fun PokemonContent_Preview_Error() {
+    PokeKotlinApiComposeSampleTheme {
+        PokemonContent(
+            pokemonIdText = "",
+            isEnableButton = false,
+            onValueChange = {},
+            uiState = UiState.Error("error!!!!"),
+            onClickSearchButton = {},
+            showSnackBarMessage = {}
+        )
+    }
+}
+
+@Preview(
+    "default: Not Empty",
+    showBackground = true,
+    uiMode = UI_MODE_NIGHT_NO,
+    device = Devices.PIXEL_4
+)
+@Composable
+fun PokemonContent_Preview_NotEmpty() {
+    PokeKotlinApiComposeSampleTheme {
+        PokemonContent(
+            pokemonIdText = "abc",
+            isEnableButton = true,
+            onValueChange = {},
+            uiState = UiState.PokemonData(id = 1, name = "abc", url = null),
+            onClickSearchButton = {},
+            showSnackBarMessage = {}
+        )
+    }
+}
+
+@Preview(
+    "default: Empty",
+    showBackground = true,
+    uiMode = UI_MODE_NIGHT_NO,
+    device = Devices.PIXEL_4
+)
+@Composable
+fun PokemonContent_Preview_Empty() {
+    PokeKotlinApiComposeSampleTheme {
+        PokemonContent(
+            pokemonIdText = "",
+            isEnableButton = false,
+            onValueChange = {},
+            uiState = UiState.Initial,
+            onClickSearchButton = {},
+            showSnackBarMessage = {}
+        )
+    }
+}
---
+++ PokeKotlinApiComposeSample/app/src/main/java/com/example/pokekotlinapicomposesample/PokemonViewModel.kt
@@ -0,0 +1,39 @@
+package com.example.pokekotlinapicomposesample
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+import me.sargunvohra.lib.pokekotlin.client.PokeApiClient
+
+import me.sargunvohra.lib.pokekotlin.client.PokeApi
+
+class PokemonViewModel() : ViewModel() {
+    var uiState by mutableStateOf<UiState>(UiState.Initial)
+        private set
+
+    fun searchPokemon(id: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            kotlin.runCatching {
+                val pokeApi: PokeApi = PokeApiClient()
+                pokeApi.getPokemon(id.toInt())
+            }.onSuccess {
+                uiState =
+                    UiState.PokemonData(id = it.id, name = it.name, url = it.sprites.backDefault)
+            }.onFailure {
+                uiState = UiState.Error(it.localizedMessage ?: "error!")
+            }
+        }
+    }
+}
+
+sealed interface UiState {
+    object Initial : UiState
+    data class PokemonData(val id: Int, val name: String, val url: String?) : UiState
+    data class Error(val errorMessage: String) : UiState
+}
